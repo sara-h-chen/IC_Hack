b3fd444004f8ccc632764e452a271702b147515a
Jacek Burys <jsb314@ic.ac.uk>
2016-02-20 21:39:10 +0000

    deleted all

--- yoda/app/scripts/controllers.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('confusionApp')
-
-.controller('ChatController', ['$scope' function($scope) {
-  
-}])
-;
--- yoda/app/scripts/services.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('confusionApp')
-
-.constant("baseURL", "http://localhost:3000/")
-;

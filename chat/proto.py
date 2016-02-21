3264a3ac6d6570eea99362375a8a09e02b32075a
Jacek Burys <jsb314@ic.ac.uk>
2016-02-21 04:01:11 +0000

    even better regex

--- chat/proto.py
+++ chat/proto.py
@@ -2,14 +2,14 @@
 import re
 
 person = ['\si\s', '\syou\s', '\she\s', '\sshe\s', '\sit\s', '\sthey\s', '\swe\s']
-person_possessive = ['my\s', 'mine\s', 'yours\s', 'ours\s', 'theirs\s', 'his\s', 'hers\s']
-pronoun = ['me\s', 'you\s', 'him\s', 'her\s', 'us\s', 'them\s']
-verbs = ['\s' + verb.rstrip('\n') + '\s' for verb in open('verbs.txt')]
+person_possessive = ['\smy\s', '\smine\s', '\syours\s', '\sours\s', '\stheirs\s', '\shis\s', '\shers\s']
+pronoun = ['\sme\s', '\syou\s', '\shim\s', '\sher\s', '\sus\s', '\sthem\s']
+verbs = ['\s' + verb.rstrip('\n') for verb in open('verbs.txt')]
 
 pat_person = '(' + '|'.join(person) + ')'
 pat_person_possessive = '(' + '|'.join(person_possessive) + ')'
 pat_pronoun = '(' + '|'.join(pronoun) + ')'
-pat_verb = '(( have )?(' + '|'.join(verbs) + '))'
+pat_verb = '(( have )?( not )?(' + '|'.join(verbs) + ')(\s|ed\s)( not )?)'
 
 pat_end = '(.*)\.?'
 pat_start = '(.*)'
@@ -20,19 +20,46 @@ pat_modal = '(' + '|'.join(modal) + ')?'
 pat_text = pat_start + '(' +pat_person+ pat_modal +pat_verb + ')' +pat_end
 pat = re.compile(pat_text)
 
+pat_question_text = pat_start + pat_verb + pat_person + pat_end
+pat_question = re.compile(pat_question_text)
+
+pat_no_person_text = pat_start + '(' + pat_modal + pat_verb + ')' + pat_end
+pat_no_person = re.compile(pat_no_person_text)
+
 while True:
   text = ' ' + input()
   text = re.sub(' ', '  ', text)
   print(text)
   match = re.search(pat, text)
-  n = len(match.groups())
   if match:
-    #print(match.groups())
+    n = len(match.groups())
     for i in range(n+1):
       print(i, " : ",  match.group(i))
-    result = match.group(1) + ' ' + match.group(8) + ' ' + match.group(2)
+    result = match.group(1) + ' ' + match.group(11) + ' ' + match.group(2)
     print(result)
     result = re.sub('\s+', ' ', result)
     print(result)
-  else:
-    print(text)
+    continue
+  match = re.search(pat_question, text)
+  if match:
+    #question
+    n = len(match.groups())
+    for i in range(n+1):
+      print(i, " : ",  match.group(i))
+    result = match.group(6) + ' ' + match.group(2) + ' ' + match.group(5)
+    print(result)
+    result = re.sub('\s+', ' ', result)
+    print(result)
+    continue
+  match = re.search(pat_no_person, text)
+  if match:
+    # no person
+    n = len(match.groups())
+    for i in range(n+1):
+      print(i, " : ",  match.group(i))
+    result = match.group(10) + ' ' + match.group(2) + ' ' + match.group(1)
+    print(result)
+    result = re.sub('\s+', ' ', result)
+    print(result)
+    continue
+  print(text)
